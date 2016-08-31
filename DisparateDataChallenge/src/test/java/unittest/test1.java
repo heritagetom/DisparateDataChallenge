cb1054ba1a58d2920e442243a0ce138414ecb35f
Jake Tutmaher <Tutmaher_Jacob@bah.com>
2016-08-31 17:17:40 -0400

    Made Updates to dv.java class in challenge.waterquality package.

--- DisparateDataChallenge/src/test/java/unittest/test1.java
+++ DisparateDataChallenge/src/test/java/unittest/test1.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.apache.commons.io.FileUtils;
-import challenge.database.dv;
+
+import challenge.waterquality.dv;
 
 public class test1 {
 	
 	public static void main(String[] args) throws IOException, InterruptedException{
-		String parentdir = "/Users/jacobtutmaher/Desktop/";
+		//Test Database functionality
 		dv testDatabase = new dv();
-		String mainstring = testDatabase.database();
-		mainstring += testDatabase.addState("NY");
-		mainstring += testDatabase.addSite(null);
-		mainstring += testDatabase.addCounty(null);
-		mainstring = mainstring.substring(0,mainstring.length()-1);
-		System.out.println(mainstring);
+		testDatabase.addState("PA");
+		testDatabase.addSite(null);
+		testDatabase.addCounty(16350);
+		System.out.println(testDatabase.getPath());
 		
-		File file = new File(parentdir+"test.txt");
-		URL url = new URL(mainstring);
-		FileUtils.copyURLToFile(url,file);
+		//String parentdir = "/Users/jacobtutmaher/Desktop/";
+		//File file = new File(parentdir+"test.txt");
+		//URL url = new URL(mainstring);
+		//FileUtils.copyURLToFile(url,file);
 		
 	}
 
