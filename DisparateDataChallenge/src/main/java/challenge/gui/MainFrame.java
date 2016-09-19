be944ace8c23df3909f0f33720573e54a9a1b9e7
heritagetom <tomheritagev@gmail.com>
2016-09-19 16:06:10 -0400

    preparing merge

--- DisparateDataChallenge/src/main/java/challenge/gui/MainFrame.java
+++ DisparateDataChallenge/src/main/java/challenge/gui/MainFrame.java
@@ -38,22 +38,27 @@ public class MainFrame {
 	JLabel lblDatabase = new JLabel("Database:");
 	JLabel lblDatalink = new JLabel("Datalink:");
 	JLabel lblDataSets = new JLabel("Data Sets:");
-	
+
 	//comboboxes
 	JComboBox<String> comboBox_Database = new JComboBox<String>();
 	JComboBox<String> comboBox_DataLink= new JComboBox<String>();
 	JComboBox<String> comboBox_DataSet = new JComboBox<String>();
-	
+
 	//buttons
-	JButton btnSelectFilters = new JButton("Select Filters");
+	JButton btn_AnalyzeData = new JButton("Analyze Data");
+	JButton btn_AddDataSet = new JButton("Add Data File");
+
 
 	//declare class variables
 	final String LOCALHOST = "http://localhost:9200";
 	String dataIndex, dataType, dataFile= null;
-    String[] fileNames;
+	String[] fileNames;
 	StringBuilder path = new StringBuilder("");
 	int fileIndex=0;
-	
+
+	//determines if kibana window is open
+	boolean kibanaOpen = false;
+
 	//getters
 	public String getDataBaseName()
 	{
@@ -68,7 +73,7 @@ public class MainFrame {
 		return dataFile;
 	}
 
-	
+
 	// Launch the application,start splash screen and delay main frame from 
 	//starting for three seconds, closing the splash screen before doing so
 	//instantiating the Main Frame object will call the main method, initialize
@@ -105,10 +110,9 @@ public class MainFrame {
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.setTitle("NGA Database Search");
-		comboBox_Database.setFont(new Font("DecoType Naskh", Font.BOLD, 13));
-		
 
-		
+
+
 		//Main Database Action Listener
 		//On first click, show Data Link and Data Set comboxboxes
 		//Populate Data Link combobox box based on Database selection
@@ -117,67 +121,71 @@ public class MainFrame {
 			public void actionPerformed(ActionEvent e) 
 			{
 				JComboBox<String> comboBox = (JComboBox) e.getSource();
-                String selected = (String)comboBox.getSelectedItem();
-                lblDatalink.setVisible(true);
-            	comboBox_DataLink.setVisible(true);            	
+				String selected = (String)comboBox.getSelectedItem();
+				lblDatalink.setVisible(true);
+				lblDataSets.setVisible(true);
+				comboBox_DataSet.setVisible(true);  
+				comboBox_DataLink.setVisible(true);
+
 				switch(selected)
 				{
-					case "Other":
-	                {
-	            		comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Water Quality Data", "Offline Map Service User Documentation", "Hazard Specific Data", "weater.mfc.nasa.gov/Goes/"}));
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {"Instantaneous Values Web Service", "Site Service", "Daily Values Web Service", "Water Quality Web Services", "Groundwater Levels Web Service", "Statistics Web Service"}));
-	                	break;
-	                }
-					case "WMS & WFS":
-					{
-						comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Nigeria Maps,Water Table", "Disaster Response- NEPAL", "Wildlife Trafficking", "Geonames- foreign", "Navy BlueMarble- low res (WMS)", "Navy BlueMarble- low res (WMTS)"})); 
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
+				case "Other":
+				{
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Water Quality Data", "Offline Map Service User Documentation", "Hazard Specific Data", "weater.mfc.nasa.gov/Goes/"}));
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {"Instantaneous Values Web Service", "Site Service", "Daily Values Web Service", "Water Quality Web Services", "Groundwater Levels Web Service", "Statistics Web Service"}));
+					comboBox_DataSet.setVisible(true);            	
 
-	                	break;
-					}
-					case "NITF Data":
-					{
-						comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Sample NITF Data"})); 
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
-	                	break;
-					}
-					
-					case "Imagery":
-					{		
-						comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"FAA Airports", "LANSAT Data", "AsiaMaritime Transparency (Public service)", "Bathymetric, GeophysicalMaps(NOAA)"}));  
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
-	                	break;
-					}
-					case "Humanitarian Data":
-					{
-						comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"West Africa Ebola Outbreak", "Data for World Food Program", "UN Humanitarian response", "Humanitarian Open Street Map", "Humanitarian Data Exchange", "U.S. Fire Data"}));
-	                	
-	                	break;
-					}
-					
-					case "Raw Data":
-					{
-						comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Practical Exercise", "Image File (Courtesy of Digital Globe)", "LiDar", "Phase 1", "Raw Imagery (Courtesy of Digital Globe)"}));
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
-	                	break;
-					}
+					break;
 				}
-                lblDataSets.setVisible(true);
+				case "WMS & WFS":
+				{
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Nigeria Maps,Water Table", "Disaster Response- NEPAL", "Wildlife Trafficking", "Geonames- foreign", "Navy BlueMarble- low res (WMS)", "Navy BlueMarble- low res (WMTS)"})); 
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {""}));
+
+					break;
+				}
+				case "NITF Data":
+				{
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Sample NITF Data"})); 
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {""}));
+					break;
+				}
+
+				case "Imagery":
+				{		
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"FAA Airports", "LANSAT Data", "AsiaMaritime Transparency (Public service)", "Bathymetric, GeophysicalMaps(NOAA)"}));  
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {""}));
+					break;
+				}
+				case "Humanitarian Data":
+				{
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"West Africa Ebola Outbreak", "Data for World Food Program", "UN Humanitarian response", "Humanitarian Open Street Map", "Humanitarian Data Exchange", "U.S. Fire Data"}));
+					comboBox_DataSet.setVisible(true);            	
+					break;
+				}
+
+				case "Raw Data":
+				{
+					comboBox_DataLink.setModel(new DefaultComboBoxModel(new String[] {"Practical Exercise", "Image File (Courtesy of Digital Globe)", "LiDar", "Phase 1", "Raw Imagery (Courtesy of Digital Globe)"}));
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
+					break;
+				}
+				}
 				if(comboBox_DataSet.getItemCount()>0)
 				{
-                	comboBox_DataSet.setVisible(true);
-                	btnSelectFilters.setEnabled(true);
+					comboBox_DataSet.setVisible(true);
+					btn_AnalyzeData.setEnabled(true);
+					lblDataSets.setVisible(true);
 				}
 				else
 				{
 					comboBox_DataSet.setVisible(false);
-                	btnSelectFilters.setEnabled(false);
+					btn_AnalyzeData.setEnabled(false);
 				}
-					
+
 			}
 		});
-		comboBox_DataLink.setFont(new Font("DecoType Naskh", Font.BOLD, 13));
-		
+
 		//Similar to the Database action listener, however this time, in the Data Link listener we populate the Data Sets combo box based on the 
 		//Data Link selection
 		//Default case refers to any case we did not populate yet
@@ -187,70 +195,75 @@ public class MainFrame {
 			{
 				FileNameLoader loader;
 				JComboBox<String> comboBox = (JComboBox) e.getSource();
-                String selected = (String)comboBox.getSelectedItem();
-                
-                dataIndex= comboBox_Database.getSelectedItem().toString().replaceAll("\\s+","");
-				dataType = comboBox_DataLink.getSelectedItem().toString().replaceAll("\\s+","");
-				path.append("./resources/"+ dataIndex + "/" + dataType+"/");
-				System.out.println(path.toString());
-		
-                
-                File[] files;
-                switch(selected)
-                {
-	                case "Water Quality Data":
-	                {	
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {"Instantaneous Values Web Service", "Site Service", "Daily Values Web Service", "Water Quality Web Services", "Groundwater Levels Web Service", "Statistics Web Service"}));
-	                	break;
-	                }
-	                case "West Africa Ebola Outbreak":
-	                {
-	                	loader = new FileNameLoader(path.toString());
-	                	files = loader.getNames();
-	                	System.out.println("Hi Don");
-	                	fileNames = new String[files.length-1];
-	                	int index =0;
-	                	for(File f:files)
-	                	{
-	                		if(f.getName().startsWith("."))
-	                			continue;
-	                		else
-	                		{
-	                			fileNames[index]=f.getName().toString();
-	                			index++;
-	                		}
-	                		
-	                	}
-	                	//populateComboBox;
-            			comboBox_DataSet.setModel(new DefaultComboBoxModel(fileNames));
-	                	break;
-	                }
-	                default:
-	                {
-	            		comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
-	            		break;
+				String selected = (String)comboBox.getSelectedItem();
 
-	                }
-                }
-                if(comboBox_DataSet.getItemCount()>0)
+				dataIndex= comboBox_Database.getSelectedItem().toString().replaceAll("\\s+","");
+				dataType = comboBox_DataLink.getSelectedItem().toString().replaceAll("\\s+","");
+				path.append("src/main/resources/"+ dataIndex + "/" + dataType+"/");
+				System.out.println(path.toString());
+
+
+				File[] files;
+				
+				
+				
+
+				switch(selected)
 				{
-                	comboBox_DataSet.setVisible(true);
-                	btnSelectFilters.setEnabled(true);
+				case "Water Quality Data":
+				{	
+					comboBox_DataSet.setVisible(true);
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {"Instantaneous Values Web Service", "Site Service", "Daily Values Web Service", "Water Quality Web Services", "Groundwater Levels Web Service", "Statistics Web Service"}));
+					break;
+				}
+				case "West Africa Ebola Outbreak":
+				{
+					loader = new FileNameLoader(path.toString());
+					files = loader.getNames();
+					fileNames = new String[files.length-1];
+					int index =0;
+					for(File f:files)
+					{
+						if(f.getName().startsWith("."))
+							continue;
+						else
+						{
+							fileNames[index]=f.getName().toString();
+							index++;
+						}
+
+					}
+					//populateComboBox;
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(fileNames));
+					comboBox_DataSet.setVisible(true);
+					break;
+				}
+				default:
+				{
+					comboBox_DataSet.setVisible(false);
+					comboBox_DataSet.setModel(new DefaultComboBoxModel(new String[] {}));
+					break;
+
+				}
+				}
+				if(comboBox_DataSet.getItemCount()>0)
+				{
+					comboBox_DataSet.setVisible(true);
+					btn_AnalyzeData.setEnabled(true);
 				}
 				else
 				{
 					comboBox_DataSet.setVisible(false);
-                	btnSelectFilters.setEnabled(false);
+					btn_AnalyzeData.setEnabled(false);
 				}
 
-						
+
 			}
 		});
-						
-		
-		btnSelectFilters.setEnabled(false);
-		
-		btnSelectFilters.addActionListener(new ActionListener() 
+		btn_AnalyzeData.setAutoscrolls(true);
+		btn_AnalyzeData.setEnabled(false);
+
+		btn_AnalyzeData.addActionListener(new ActionListener() 
 		{
 			public void actionPerformed(ActionEvent e) 
 			{
@@ -265,18 +278,23 @@ public class MainFrame {
 					dataType = comboBox_DataLink.getSelectedItem().toString().replaceAll("\\s+","").toLowerCase();
 					dataFile = comboBox_DataSet.getSelectedItem().toString();
 
-					//Hide frame
-					frame.setVisible(false);
 
 					//Run main application
 					MainApplication main = new MainApplication();
-					path.append(dataFile);
-					
+					path.append(dataFile);		
+					comboBox_Database.setEnabled(false);
+					comboBox_DataLink.setEnabled(false);
+					comboBox_DataSet.setEnabled(false);
+					btn_AnalyzeData.setVisible(false);
+					btn_AnalyzeData.setEnabled(false);
+					btn_AddDataSet.setVisible(true);
+					btn_AddDataSet.setEnabled(true);
 
-					if(Desktop.isDesktopSupported()){
+					if(!kibanaOpen && Desktop.isDesktopSupported()){
 						Desktop desktop = Desktop.getDesktop();
 						try {
 							desktop.browse(new URI("http://localhost:5601/app/kibana"));
+							kibanaOpen = true;
 						} catch (IOException | URISyntaxException ioex) {
 							// TODO Auto-generated catch block
 							ioex.printStackTrace();
@@ -284,14 +302,33 @@ public class MainFrame {
 					}
 					main.run(dataIndex, dataType,path.toString());
 
-						//FilterSelection.show(dataBase, dataLink, dataSet);
-					}
+					//FilterSelection.show(dataBase, dataLink, dataSet);
 				}
-			});
+			}
+		});
+		btn_AddDataSet.setVisible(false);
+		btn_AddDataSet.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btn_AddDataSet.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						btn_AddDataSet.setVisible(false);
+						btn_AddDataSet.setEnabled(false);
+						btn_AnalyzeData.setVisible(true);
+						btn_AnalyzeData.setEnabled(true);
+						comboBox_Database.setEnabled(true);
+						comboBox_DataLink.setEnabled(true);
+						comboBox_DataSet.setEnabled(true);
+					}
+				});
+			}
+		});
+
+
+
 		frame.getContentPane().setLayout(null);
 		
-		
-		
+
+
 		//DataBase combobox and label config
 		comboBox_Database.setModel(new DefaultComboBoxModel(new String[] {"WMS & WFS", "NITF Data", "Imagery", "Humanitarian Data", "Other", "Raw Data"}));
 		comboBox_Database.setToolTipText("");
@@ -299,7 +336,7 @@ public class MainFrame {
 		frame.getContentPane().add(comboBox_Database);
 		lblDatabase.setBounds(98, 56, 74, 16);
 		frame.getContentPane().add(lblDatabase);
-		
+
 		//DataLink combobox and label config
 		lblDataSets.setVisible(false);
 		comboBox_DataLink.setVisible(false);
@@ -310,7 +347,7 @@ public class MainFrame {
 		comboBox_DataLink.setBounds(197, 84, 151, 27);
 		frame.getContentPane().add(comboBox_DataLink);
 
-		
+
 		//DataSets combobox and label config
 		lblDataSets.setVisible(false);
 		comboBox_DataSet.setFont(new Font("DecoType Naskh", Font.BOLD, 13));
@@ -321,8 +358,14 @@ public class MainFrame {
 		comboBox_DataSet.setToolTipText("");
 		comboBox_DataSet.setBounds(197, 112, 151, 27);
 		frame.getContentPane().add(comboBox_DataSet);
-		btnSelectFilters.setBounds(141, 144, 124, 29);
-		frame.getContentPane().add(btnSelectFilters);
+		btn_AnalyzeData.setBounds(151, 144, 124, 29);
+		frame.getContentPane().add(btn_AnalyzeData);
+		btn_AddDataSet.setBounds(151, 144, 124, 29);
+		frame.getContentPane().add(btn_AddDataSet);
+
+	}
+	private void checkComboBox() {
+		// TODO Auto-generated method stub
 		
 	}
 }
